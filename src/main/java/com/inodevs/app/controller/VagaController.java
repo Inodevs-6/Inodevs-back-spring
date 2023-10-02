a0a4cb30d644606310a83b1cd0817178ef4f9bd7
Richardrafael <richard.rafael.soares@gmail.com>
2023-10-02 19:12:31 -0300

    feat: all Vagas

--- src/main/java/com/inodevs/app/controller/VagaController.java
+++ src/main/java/com/inodevs/app/controller/VagaController.java
@@ -1,7 +1,10 @@
 package com.inodevs.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,11 @@ public class VagaController {
     @PostMapping
     public Vaga novaVaga(@RequestBody Vaga vaga) {
         return vagaService.novaVaga(vaga);
+    }
+
+    @GetMapping
+    public List<Vaga> todosVagas() {
+        return vagaService.buscarTodosVagas();
     }
 
 }
--- src/main/java/com/inodevs/app/service/VagaService.java
+++ src/main/java/com/inodevs/app/service/VagaService.java
@@ -1,20 +1,24 @@
 package com.inodevs.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.inodevs.app.entity.Vaga;
 import com.inodevs.app.repository.VagaRepository;
 
 @Service
 public class VagaService {
-    
+
     @Autowired
     private VagaRepository vagaRepository;
 
-    public Vaga novaVaga (Vaga vaga) {
-        
-        if(vaga == null ||
+    public Vaga novaVaga(Vaga vaga) {
+
+        if (vaga == null ||
                 vaga.getNome() == null ||
                 vaga.getNome().isBlank() ||
                 vaga.getNivel() == null ||
@@ -22,7 +26,15 @@ public class VagaService {
             throw new IllegalArgumentException("Os campos obrigatórios não foram preenchidos!");
         }
         return vagaRepository.save(vaga);
-        
+
+    }
+
+    public List<Vaga> buscarTodosVagas() {
+        try {
+            return vagaRepository.findAll();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erro ao listar as Vagas!");
+        }
     }
 
 }
